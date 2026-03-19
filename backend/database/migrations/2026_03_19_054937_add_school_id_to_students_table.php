494d279c360d9ad24b27f35fd467f3f2fbcf72c1
Mathavik <mathavikanagaraj1@gmail.com>
2026-03-19 11:36:15 +0530

    schooltable

---
+++ backend/database/migrations/2026_03_19_054937_add_school_id_to_students_table.php
@@ -0,0 +1,20 @@
+<?php
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up()
+    {
+        Schema::table('students', function (Blueprint $table) {
+            $table->unsignedBigInteger('school_id')->nullable(); // ✅ no error
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('students', function (Blueprint $table) {
+            $table->dropColumn('school_id');
+        });
+    }
+};
